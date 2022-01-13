112b32ff953ecd7a6a8bd00e1aded2e4ff33136c
awab322 <awabmehar869@gmail.com>
2022-01-14 01:40:01 +0500

    Some Changes

--- src/components/EnterData.jsx
+++ src/components/EnterData.jsx
@@ -1,27 +1,32 @@
-import {React , useState} from 'react'
+import React,{useState} from 'react'
 import { depositMoney , withdrawMoney } from "../state/actions/index";
 import {useDispatch} from "react-redux"
  
 const EnterData = () => {
 
+const [money,setMoney] = useState("");
+
+function getMoney(aa) {
+    setMoney(aa.target.value)
+}
 
 const dispatch = useDispatch();
 
     return (
         <>
            <div className="card">
-                <h1>Enter Data</h1> 
-                {/* <input className="cash_input shadow-none" type="number" placeholder="Enter Amount"/> */}
+                {/* <h1>Enter Data</h1>  */}
+                <input className="cash_input shadow-none" onChange={getMoney} value={money} type="number" placeholder="Enter Amount"/>
 
-                <button className="btn btn-block shadow-none add_button" 
-                onClick={()=>dispatch(depositMoney(50))}
+                <button className="btn btn-block shadow-none add_button"
+                onClick={()=>dispatch(depositMoney(Number(money),setMoney("")))}
                 >
-                Deposit Money (50)
+                Deposit Money
                 </button>
                 <button className="btn btn-block shadow-none remove_button"
-                onClick={()=>dispatch(withdrawMoney(25))}
+                onClick={()=>dispatch(withdrawMoney(Number(money),setMoney("")))}
                 >
-                Withdraw Money (25)
+                Withdraw Money
                 </button>
 
             </div> 
